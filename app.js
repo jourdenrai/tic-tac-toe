ca05a259d572f96078c33638cfc36eb1fa5b5192
Jourden <jourdenrai@gmail.com>
2020-06-24 01:44:34 -0500

    added full functionality of tic-tac-toe game, cleaned up code

--- app.js
+++ app.js
@@ -3,36 +3,43 @@ const GameBoard = (() => {
     return gameBoard
 })();
 
-const player = (name) => {
-    return {name}
-}
+
 var playerTurn = true
 var board = document.querySelector("#board")
-    var player1 = prompt('Player 1:')
-    var player2 = prompt('Player 2:')
-    var names = document.querySelector('#names')
+var reset = document.querySelector('#reset')
 
-    var reset = document.querySelector('#reset')
-    reset.addEventListener('click',() => {
-        
-    })
+var num = 0
+
+var winArray = [[0,1,2],[3,4,5],[6,7,8],
+                    [0,3,6],[1,4,7],[2,5,8],
+                    [0,4,8],[2,4,6]]; 
 
 function render(){
-    
-    names.textContent = `${player1} is X, ${player2} is O`
+    //make grid
     GameBoard.forEach(box =>{
+        
         var element = document.createElement('div')
         element.classList.add('element')
+        element.id = num
         element.classList.add('content')
         element.textContent = box
         board.appendChild(element)
+        num++
+
     })
     var boardPieces = document.querySelectorAll('.element')
+    
     boardPieces.forEach((piece) =>{
+        
+        reset.addEventListener('click',() =>{
+                    location.reload()
+                        // piece.textContent = ' '
+                        // playerTurn = true
+                    })
+                
         piece.addEventListener('click',(e)=>{
             
             if(playerTurn == true){
-                
                 if(e.target.textContent === ' '){
                     var choice = 'x'
                     e.target.textContent = choice
@@ -40,18 +47,35 @@ function render(){
                 }
             }
             else{
-                
                 if(e.target.textContent === ' '){
                     var choice = 'o'
                     e.target.textContent = choice
                     playerTurn = true
-                } 
-                
+                }  
             }
             
-        })
-        
-    })
+            for(let i = 0;i < winArray.length;i++){
+                var xCount = 0
+                var oCount = 0
+                for(let j = 0; j < winArray[i].length;j++){
+                    if(boardPieces[winArray[i][j]].textContent === 'x'){
+                        xCount++
+                        if(xCount === 3){
+                            alert('X wins')
+                            location.reload()
+                        }   
+                    }
+                    else if(boardPieces[winArray[i][j]].textContent === 'o'){
+                        oCount--
+                        if(oCount === -3){
+                            alert('O wins')
+                            location.reload()
+                        }
+                    }
+                }
+            }
+        })       
+    })  
 }
 
 render()
